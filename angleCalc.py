5c588137f5501291b3f60cd10fd4ecca6a5f9d40
Laurence Ruberl <bknd225@gmail.com>
2017-05-18 17:16:18 -0400

    Initial commit

---
+++ angleCalc.py
@@ -0,0 +1,11 @@
+
+
+class rocket:
+    def __init__(self, filePath):
+        with open(filePath, 'r') as keyFile:
+            contents = keyFile.read().split
+            print(contents)
+
+
+
+def angleCalc(rocketFile, conditions, distance):
