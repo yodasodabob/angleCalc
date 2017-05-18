47cdfc11b1cb6258bbfe131531da47f67795a2f4
Laurence Ruberl <bknd225@gmail.com>
2017-05-18 17:53:59 -0400

    Begin setting up rocket class

--- angleCalc.py
+++ angleCalc.py
@@ -1,11 +1,14 @@
-
+from scipy import *
 
 class rocket:
     def __init__(self, filePath):
         with open(filePath, 'r') as keyFile:
             contents = keyFile.read().split
-            print(contents)
+            print contents
+            self.name = contents[0].split(' ')[2:]
 
 
 
-def angleCalc(rocketFile, conditions, distance):
+
+def angleCalc(rocketFile, conditionsFile, distance):
+    print('Do Stuff')
